eb000179576572807d15246eca62e04e9dc1ef4c
Rukshan Dissanayake <rukshanmali@gmail.com>
2023-12-02 22:18:34 +0530

    Method created for log in and number entering

--- src/GuessNumber.java
+++ src/GuessNumber.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
 
 public class GuessNumber {
 
+	private static Scanner scan = new Scanner(System.in);
+	
+	
 	public static void main(String[] args) {
+		System.out.println("Hello  Player!! Welcome to the Number Guessing Game");
+		
+		
+		logIn();
 
+	}
+	
+	public static void logIn() {
+		System.out.println("\nEnter Your Name : ");
+		String name = scan.nextLine();
+		System.out.println("\nHi " +name + " Guess any number between 1-100 : ");
+		int num = scan.nextInt();
 	}
 
 }
